b2e7b4ce1f987b7e79f8b90e4ae23d6d4007044c
Fabian Höring <f.horing@criteo.com>
2019-09-19 17:44:45 +0200

    Reduce gradients one by one in DistributedOptimizer
    - tf allgather/allreduce can't reduce arrays of tensors with different types

--- tf_collective_ops/python/ops/optimizer_wrapper.py
+++ tf_collective_ops/python/ops/optimizer_wrapper.py
@@ -3,7 +3,7 @@ from tf_collective_ops import allreduce, allgather
 
 
 class DistributedOptimizer(tf.train.Optimizer):
-    
+
     def __init__(self, optimizer, n_workers, name=None, use_locking=False, average=True):
         """Construct a new DistributedOptimizer, which uses another optimizer
         under the hood for computing single-process gradient values and
@@ -24,55 +24,46 @@ class DistributedOptimizer(tf.train.Optimizer):
         self.n_workers = n_workers
         self.average = average
 
-        def allreduce_grads(grads_vars):
-            def op_with_dependencies(dependencies, op, grads):
-                with tf.control_dependencies(dependencies):
-                    res = op(grads)
-                dependencies += res if isinstance(res, list) else list(res)
-                return res
-
-            def _allgather(dependencies, grads):
-                return op_with_dependencies(dependencies, allgather, grads)
-
-            def _allreduce(dependencies, grads):
-                return op_with_dependencies(dependencies, allreduce, grads)
-
-            dependencies = []
-            grads_vars_to_gather = []
-            grads_vars_to_reduce = []
-            for grad, var in grads_vars:
-                if isinstance(grad, tf.IndexedSlices):
-                    grads_vars_to_gather.append((grad, var))
-                else:
-                    grads_vars_to_reduce.append((grad, var))
-
-            new_grads_vars = []
-
-            if len(grads_vars_to_gather) > 0:
-                grads, _ = zip(*grads_vars_to_gather)
-                gathered_indices = _allgather(dependencies, [grad.indices for grad in grads])
-                gathered_values = _allgather(dependencies, [grad.values for grad in grads])
-                if self.average:
-                    gathered_values = [tf.div(gathered_value, self.n_workers) for gathered_value in gathered_values]
-                new_grads_vars.extend([
-                    (tf.IndexedSlices(
-                        indices=indices, values=values, dense_shape=grad.dense_shape
-                    ), var) for (grad, var), values, indices in zip(grads_vars_to_gather, gathered_values, gathered_indices)
-                ])
-
-            if len(grads_vars_to_reduce) > 0:
-                grads, _ = zip(*grads_vars_to_reduce)
-                reduced_grads = _allreduce(dependencies, grads)
-                if self.average:
-                    reduced_grads = [tf.div(reduced_grad, self.n_workers) for reduced_grad in reduced_grads]
-                new_grads_vars.extend([(reduced_grad, var) for (_, var), reduced_grad in zip(grads_vars_to_reduce, reduced_grads)])
-
-            return new_grads_vars
-
-        self._allreduce_grads = allreduce_grads
-
         super(DistributedOptimizer, self).__init__(
-            name="Myoptimizer", use_locking=use_locking)
+            name="DistributedOptimizer", use_locking=use_locking)
+
+    def _op_with_dependencies(self, dependencies, op, grads):
+        with tf.control_dependencies(dependencies):
+            res = op(grads)
+        dependencies += res if isinstance(res, list) else [res]
+        return res
+
+    def _allreduce_grad(self, dependencies, tensor):
+        n_workers = tf.cast(self.n_workers, dtype=tensor.dtype)
+        if isinstance(tensor, tf.IndexedSlices):
+            values = self._op_with_dependencies(
+                dependencies,
+                allgather,
+                [tensor.values])[0]
+            indices = self._op_with_dependencies(
+                dependencies,
+                allgather,
+                [tensor.indices])[0]
+            if self.average:
+                values = tf.div(values, n_workers)
+            return tf.IndexedSlices(
+                indices=indices,
+                values=values,
+                dense_shape=tensor.dense_shape)
+        else:
+            summed_tensor = self._op_with_dependencies(dependencies, allreduce, [tensor])[0]
+            if self.average:
+                summed_tensor = tf.div(summed_tensor, n_workers)
+            return summed_tensor
+
+    def _allreduce_grads(self, grads_vars):
+            tf.logging.info("Creating allreduce grad ops")
+            ops = []
+            dependencies = []
+            for grad in grads_vars:
+                allred_op = self._allreduce_grad(dependencies, grad)
+                ops.append(allred_op)
+            return ops
 
     def compute_gradients(self, *args, **kwargs):
         """Compute gradients of all trainable variables.
@@ -82,9 +73,11 @@ class DistributedOptimizer(tf.train.Optimizer):
         In DistributedOptimizer, compute_gradients() is overriden to also
         allreduce the gradients before returning them.
         """
-        grads_vars = self._optimizer.compute_gradients(*args, **kwargs)
+        gradients = self._optimizer.compute_gradients(*args, **kwargs)
         if self.n_workers > 1:
-            return self._allreduce_grads(grads_vars)
+            grads, vars = zip(*gradients)
+            avg_grads = self._allreduce_grads(grads)
+            return list(zip(avg_grads, vars))
         else:
             return gradients
 
