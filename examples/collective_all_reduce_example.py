2eaa1b00772b6e6fc166e83144bfe8a547dbad9e
Fabian Höring <f.horing@criteo.com>
2019-10-01 15:55:24 +0200

    Add tf-yarn support + example
    Change-Id: I13b9e79bb67145989e71ae95cce8d58684a88374

---
+++ examples/collective_all_reduce_example.py
@@ -0,0 +1,83 @@
+"""
+Example of custom distributed training with one node training and using https://github.com/dmlc/rabit AllReduce
+"""
+import logging
+import os
+import pwd
+import getpass
+from subprocess import check_output
+import skein
+import tensorflow as tf
+import winequality
+from datetime import datetime
+
+from tf_yarn import Experiment, TaskSpec, packaging, run_on_yarn
+
+from tf_collective_all_reduce import (
+    DistributedOptimizer,
+    BroadcastGlobalVariablesHook
+)
+
+logging.basicConfig(level="INFO")
+
+USER = getpass.getuser()
+
+"""
+1. Download winequality-*.csv from the Wine Quality dataset at UCI
+   ML repository
+   (https://archive.ics.uci.edu/ml/datasets/Wine+Quality).
+2. Upload it to HDFS
+3. Pass a full URI to either of the CSV files to the example
+"""
+WINE_EQUALITY_FILE = f"{packaging.get_default_fs()}/user/{USER}/tf_yarn_test/winequality-red.csv"
+
+"""
+Output path of the learned model on hdfs
+"""
+HDFS_DIR = (f"{packaging.get_default_fs()}/user/{USER}"
+            f"/tf_yarn_test/tf_yarn_{int(datetime.now().timestamp())}")
+
+NB_WORKERS = 1
+
+
+def experiment_fn() -> Experiment:
+    def train_input_fn():
+        train_data, test_data = winequality.get_train_eval_datasets(WINE_EQUALITY_FILE)
+        return (train_data.shuffle(1000)
+                .batch(128)
+                .repeat())
+
+    estimator = tf.estimator.LinearClassifier(
+        optimizer=DistributedOptimizer(tf.train.FtrlOptimizer(learning_rate=0.1), NB_WORKERS+1),
+        feature_columns=winequality.get_feature_columns(),
+        model_dir=f"{HDFS_DIR}",
+        n_classes=winequality.get_n_classes())
+
+    train_spec = tf.estimator.TrainSpec(
+        train_input_fn,
+        max_steps=1000,
+        hooks=[BroadcastGlobalVariablesHook(0)]
+    )
+    return Experiment(estimator, train_spec, tf.estimator.EvalSpec(lambda: True))
+
+
+if __name__ == "__main__":
+
+    pyenv_zip_path, env_name = packaging.upload_env_to_hdfs()
+    editable_requirements = packaging.get_editable_requirements_from_current_venv()
+
+    with skein.Client() as client:
+        run_on_yarn(
+            pyenv_zip_path,
+            experiment_fn,
+            task_specs={
+                "chief": TaskSpec(memory="1 GiB", vcores=1),
+                "worker": TaskSpec(memory="1 GiB", vcores=1, instances=NB_WORKERS)
+            },
+            files={
+                **editable_requirements,
+                os.path.basename(winequality.__file__): winequality.__file__,
+            },
+            skein_client=client,
+            custom_task_module="tf_collective_all_reduce.python.tf_yarn._rabit_allred_task"
+        )
---
+++ tf_collective_all_reduce/python/tf_yarn/_rabit_allred_task.py
@@ -0,0 +1,74 @@
+import logging
+import tensorflow as tf
+import os
+import skein
+from threading import Thread
+
+from tf_yarn import _task_commons, cluster, event
+from tf_yarn.incubator import tracker
+
+logger = logging.getLogger(__name__)
+
+
+def _setup_tracker(client):
+    host_port = event.wait(client, "chief:0/tracker")
+    tf.logging.info(f"Got tracker url {host_port}")
+    host, port = host_port.split(":")
+    os.environ['DMLC_TRACKER_URI'] = host
+    os.environ['DMLC_TRACKER_PORT'] = port
+
+
+def _start_tracker(client, n_workers: int):
+    tf.logging.info(f"Starting tracker with {n_workers} workers")
+    rabit_context = tracker.RabitTracker(
+        hostIP=tracker.get_host_ip(),
+        nslave=n_workers)
+    rabit_context.start(n_workers)
+    thread = Thread(target=rabit_context.join, daemon=True)
+    thread.start()
+
+    event.broadcast(
+        client,
+        f"{cluster.get_task()}/tracker",
+        f"{rabit_context.hostIP}:{rabit_context.port}"
+    )
+    return thread
+
+
+def main():
+    client = skein.ApplicationClient.from_current()
+    task_type, task_id = cluster.get_task_description()
+    task = cluster.get_task()
+    event.init_event(client, task, f"127.0.0.1:0")
+    _task_commons._setup_container_logs(client)
+
+    os.environ['DMLC_RANK'] = "0" if task_type == 'chief' else f"{task_id + 1}"
+    os.environ['DMLC_ROLE'] = "worker"
+
+    cluster_tasks = _task_commons._get_cluster_tasks(client)
+
+    logger.info(cluster_tasks)
+
+    if task_type == 'chief':
+        _start_tracker(client, len(cluster_tasks))
+
+    _setup_tracker(client)
+
+    experiment = _task_commons._get_experiment(client)
+
+    if task_type != 'chief':
+        experiment.estimator._model_dir = "."
+
+    logger.info(f"start training..")
+
+    experiment.estimator.train(
+        experiment.train_spec.input_fn,
+        hooks=experiment.train_spec.hooks,
+        max_steps=experiment.train_spec.max_steps)
+
+    event.stop_event(client, task, None)
+
+
+if __name__ == "__main__":
+    _task_commons._process_arguments()
+    main()
