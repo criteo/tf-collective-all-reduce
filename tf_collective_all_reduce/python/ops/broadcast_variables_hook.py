36d996d0ad505b965c86209cf57ef15790cc1c95
Fabian Höring <f.horing@criteo.com>
2019-10-14 17:29:52 +0200

    Allow to filter variables in BroadcastGlobalVariablesHook
    Change-Id: I1f00a392e3551f232ac2ab047d9b82f9b02c4340

--- tf_collective_all_reduce/python/ops/broadcast_variables_hook.py
+++ tf_collective_all_reduce/python/ops/broadcast_variables_hook.py
@@ -2,28 +2,37 @@ import tensorflow as tf
 from tf_collective_all_reduce import broadcast
 
 
-def broadcast_global_variables(root_rank):
-    print(f"tf.global_variables(): {tf.global_variables()}")
-    return broadcast_variables(tf.global_variables(), root_rank)
-
-
 def broadcast_variables(variables, root_rank):
+    tf.logging.info(f"Broadcasting variables: {variables}")
     broadcasted_vars = broadcast(root_rank, variables)
     return tf.group(*[tf.assign(var, broadcasted_var)
-                      for var, broadcasted_var in zip(variables, broadcasted_vars)])
+                    for var, broadcasted_var in zip(variables, broadcasted_vars)])
 
 
 class BroadcastGlobalVariablesHook(tf.train.SessionRunHook):
-    def __init__(self, root_rank, device=''):
+    def __init__(
+        self,
+        root_rank=0,
+        device='',
+        filtered_var_names=[]
+    ):
         super(BroadcastGlobalVariablesHook, self).__init__()
         self.root_rank = root_rank
         self.device = device
         self.bcast_op = None
+        self.filtered_var_names = filtered_var_names
 
     def begin(self):
         if not self.bcast_op or self.bcast_op.graph != tf.get_default_graph():
             with tf.device(self.device):
-                self.bcast_op = broadcast_global_variables(self.root_rank)
+                # we want to filter out partionned variables like
+                # 'embeddings:0', 'embeddings:1', 'embeddings:2', ..
+                # => we test if the filter pattern ['embeddings', ..] is part of the variable
+                variables = [var for var in tf.global_variables()
+                             if not [var_name for var_name in self.filtered_var_names
+                                     if var_name in var.name]
+                            ]
+                self.bcast_op = broadcast_variables(variables, self.root_rank)
 
     def after_create_session(self, session, coord):
         session.run(self.bcast_op)
